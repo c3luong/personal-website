21c9adf157cb0a4e2e6790c87f11679de7c62fb7
curtiss <curtissluong@gmail.com>
2016-02-07 22:20:29 -0400

    prerender working!

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');             // log requests to the console (express4)
 
-//var mongoose = require('mongoose');
-//mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'); // connect to mongoDB database on modulus.io
+app.use(require('prerender-node').set('prerenderServiceUrl', 'http://localhost:3000'));
 
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use('/public', express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use('/components', express.static(__dirname + '/public/components'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
 app.use(morgan('dev')); // log every request to the console
-app.use(require('prerender-node'));
 
+app.all('/*', function(req, res) {
+    res.sendFile('/public/index.html', { root: __dirname });
+});
+app.get('/', function (req, res) {
+    res.sendFile('/public/index.html');
+});
 
 
 app.listen(80);
-console.log("App listening on port 8080");
+console.log("App listening on port 80");
--- src/js-shared/config.js
+++ src/js-shared/config.js
@@ -3,14 +3,15 @@
     angular.module("PortfolioApplication")
         .config(appConfig);
 
-    appConfig.$inject = ["$stateProvider", "$urlRouterProvider", "$mdThemingProvider", "views"];
-    function appConfig($stateProvider, $urlRouterProvider, $mdThemingProvider, views){
+    appConfig.$inject = ["$stateProvider", "$urlRouterProvider", "$mdThemingProvider", "$locationProvider", "views"];
+    function appConfig($stateProvider, $urlRouterProvider, $mdThemingProvider, $locationProvider, views){
         $urlRouterProvider.otherwise("/homepage");
         $mdThemingProvider.theme('default').primaryPalette('green');
         // set up views
         angular.forEach(views, function(value){
             $stateProvider.state(value);
         });
+        $locationProvider.html5Mode(true).hashPrefix('!');
     }
 }());
 
